3e9aa969e635e032c3b3f319832ac4beedf8322a
Dennis Tank <72018872+DennisTank@users.noreply.github.com>
2025-03-01 18:43:07 -0500

    Blog Updated

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const blogList = document.getElementById("blog-list");
 POSTS.forEach(post => {
     const li = document.createElement("li");
-    li.innerHTML = `<a href="blog.html?id=${post}">${post}</a>`;
+    li.innerHTML = `<button onclick="window.location.href='blog.html?id=${post}'">${post}</button>`;
     blogList.appendChild(li);
 });
